4a22a9a8a1fbfd2533ba2a75765cb93a0b917701
starnnik <l.d.50@mail.ru>
2026-04-27 19:03:50 +0300

    deleted default from the grade field of Review model
    Signed-off-by: starnnik <l.d.50@mail.ru>

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -13,7 +13,7 @@ class Review(Base):
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
     comment: Mapped[str] = mapped_column(Text, nullable=True)
     comment_date: Mapped[datetime] = mapped_column(default=datetime.now, nullable=False)
-    grade: Mapped[int] = mapped_column(nullable=False, default=True)
+    grade: Mapped[int] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(default=True)
 
     user:Mapped["User"] = relationship("User", back_populates="reviews")
